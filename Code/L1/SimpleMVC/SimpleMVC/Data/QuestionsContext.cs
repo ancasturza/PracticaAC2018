472afb2a8546ce19e1a0960ae0520b23ac29dcea
tudor.teodoru <t.teodoru@levi9.com>
2018-06-18 15:38:37 +0300

    Inital commit

---
+++ Code/L1/SimpleMVC/SimpleMVC/Data/QuestionsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimpleMVC.Models;
+
+namespace SimpleMVC.Data
+{
+    public class QuestionsContext : DbContext
+    {
+        public QuestionsContext (DbContextOptions<QuestionsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SimpleMVC.Models.Question> Question { get; set; }
+    }
+}
---
+++ Code/L1/SimpleMVC/SimpleMVC/Models/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleMVC.Models
+{
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public string Text { get; set; }
+        public bool IsCorrect { get; set; }
+
+        [ForeignKey("Question")]
+        public int QuestionId { get; set; }
+
+        public Question Question { get; set; }
+    }
+}
