ad1a59f4d471d01d3cdba44e469ce0674e6e3e0c
Dragos Rosca <d.rosca@levi9.com>
2018-06-25 14:12:04 +0300

    Added beginning of lesson three

---
+++ Code/L3/ClassroomTrivia.LessonThree/ClassroomTrivia.LessonThree/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassroomTrivia.LessonThree.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        // This makes the field mandatory to fill.
+        [Required]
+        // This tells it what label it should have. The default is the property name and does not have spaces.
+        [Display(Name = "Authorization Code")]
+        // This provides some rules about what the field should accept as values.
+        [StringLength(
+            // The maximum length of the value.
+            maximumLength: 20,
+            // The error message that should be displayed in case the requirements are not met.
+            // {0} = display name;
+            // {1} = maximum length;
+            // {2} = minimum length.
+            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+            // The minimum length of the value.
+            MinimumLength = 6)]
+        public string AuthorizationCode { get; set; }
+    }
+}
---
+++ Code/L3/ClassroomTrivia.LessonThree/ClassroomTrivia.LessonThree/Startup.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ClassroomTrivia.LessonThree.Data;
+using ClassroomTrivia.LessonThree.Models;
+using ClassroomTrivia.LessonThree.Services;
+
+namespace ClassroomTrivia.LessonThree
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider service)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            // Use the service provider to get a instance of the database context object
+            var context = service.GetService<ApplicationDbContext>();
+
+            // Use the context instance to access the database and perform a migration
+            context.Database.Migrate();
+
+            // Call our CreateUserRoles method to make sure our roles exist.
+            // The method asks for a IServiceProvider parameter and we will pass the one we created earlier.
+            // Note that the method is asynchronous so we will have to wait for it.
+            CreateUserRoles(service).Wait();
+        }
+
+        // A method we will use to make sure our user roles are created if they don't exist.
+        private async Task CreateUserRoles(IServiceProvider service)
+        {
+            // Request a instance of the role manager.
+            var roleManager = service.GetRequiredService<RoleManager<IdentityRole>>();
+
+            // Create a variable to hold the result of the role manager when it creates a new role.
+            IdentityResult identityResult;
+
+            // Check if the administrator role exists.
+            // Note that because the RoleExistsAsync method is asynchronous we need to wait for the result.
+            var roleExists = await roleManager.RoleExistsAsync("Admin");
+            if (!roleExists)
+            {
+                // If our role does not exist we need to create it.
+                // First we need to create the administrator identity role.
+                var adminRole = new IdentityRole("Admin");
+                // Now we need to use the manager to create the role in the database.
+                identityResult = await roleManager.CreateAsync(adminRole);
+            }
+
+            // Check if the professor role exists.
+            roleExists = await roleManager.RoleExistsAsync("Professor");
+            if (!roleExists)
+            {
+                // If our role does not exist we need to create it.
+                // First we need to create the professor identity role.
+                var professorRole = new IdentityRole("Professor");
+                // Now we need to use the manager to create the role in the database.
+                identityResult = await roleManager.CreateAsync(professorRole);
+            }
+
+            // Check if the student role exists.
+            roleExists = await roleManager.RoleExistsAsync("Student");
+            if (!roleExists)
+            {
+                // If our role does not exist we need to create it.
+                // First we need to create the student identity role.
+                var studentRole = new IdentityRole("Student");
+                // Now we need to use the manager to create the role in the database.
+                identityResult = await roleManager.CreateAsync(studentRole);
+            }
+        }
+    }
+}
